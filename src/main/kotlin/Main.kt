d28c9243621f9e1e167bee8e03a515bf276d0901
Jan-Niclas Struewer <j.n.struewer@gmail.com>
2023-11-24 14:23:58 +0100

    finished cache db setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 import artifact.ArtifactService
+import artifact.db.Artifact
+import artifact.db.Artifacts
+import artifact.db.Version
+import artifact.model.ArtifactDto
 import artifact.model.DependencyGraphDto
 import artifact.model.ScopedDependencyDto
 import com.github.ajalt.clikt.core.CliktCommand
@@ -10,6 +14,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import libyears.LibyearCalculator
+import org.jetbrains.exposed.dao.with
+import org.jetbrains.exposed.sql.and
+import util.dbQuery
 import util.initDatabase
 import java.io.File
 import java.nio.file.Path
@@ -29,11 +36,15 @@ class Libyears : CliktCommand() {
         .path(mustExist = true, mustBeReadable = true, canBeFile = false)
         .required()
 
-    val outputPath by option(envvar = "OUTPUT_PATH", help = "Path to the folder to store the JSON results" +
-            "of the created dependency graph. If the path doesn't exist it will be created.")
-        .path(mustExist = false, mustBeReadable = true, mustBeWritable = true, canBeFile = false)
+    val outputPath by option(
+        envvar = "OUTPUT_PATH", help = "Path to the folder to store the JSON results" +
+                "of the created dependency graph. If the path doesn't exist it will be created."
+    )
+        .path(mustExist = false, canBeFile = false)
+
     override fun run() {
-        echo("Running libyears for project at $projectPath and optional db URL $dbUrl")
+        echo("Running libyears for project at $projectPath and output path $outputPath" +
+                " and optional db URL $dbUrl")
         dbUrl?.createDirectories()
         outputPath?.createDirectories()
     }
@@ -43,8 +54,8 @@ suspend fun main(args: Array<String>) {
     val libyearCommand = Libyears()
     libyearCommand.main(args)
 
-    val dbString = if(libyearCommand.dbUrl != null) {
-        "jdbc:sqlite:${libyearCommand.dbUrl.toString()}versionsCache.sqlite"
+    val dbString = if (libyearCommand.dbUrl != null) {
+        "jdbc:sqlite:${libyearCommand.dbUrl.toString()}/versionsCache.sqlite"
     } else {
         null
     }
@@ -91,7 +102,7 @@ suspend fun getLibYears(projectPath: File, outputPath: Path?, dbUrl: String?) {
     libyearCalculator.printDependencyGraph(dependencyGraphDto)
 
 
-    if(outputPath != null) {
+    if (outputPath != null) {
         val outputFile = outputPath.resolve("${Date().time}-graphResult.json").toFile()
         withContext(Dispatchers.IO) {
             outputFile.createNewFile()
@@ -103,8 +114,62 @@ suspend fun getLibYears(projectPath: File, outputPath: Path?, dbUrl: String?) {
 
 
     if (!dbUrl.isNullOrBlank()) {
-        //TODO store
-
+        dependencyGraphDto.packageManagerToScopes.values.forEach {
+            it.scopesToDependencies.values.forEach {
+                it.forEach { artifact ->
+                    recursivelyUpdateCache(artifact)
+                }
+            }
+        }
     }
 }
 
+suspend fun recursivelyUpdateCache(artifactDto: ArtifactDto) {
+    dbQuery {
+        val artifacts = Artifact.find {
+            Artifacts.artifactId eq artifactDto.artifactId and (Artifacts.groupId eq artifactDto.groupId)
+        }.with(Artifact::versions)
+
+        if (artifacts.count() > 1) {
+            println(
+                "The cache should only contain a single entry for every groupId and " +
+                        "artifact id combo. Clearing cache for ${artifactDto.groupId}/${artifactDto.artifactId}"
+            )
+            // TODO: we need to test if the artifacts iterable is empty after delete
+            artifacts.forEach { it.delete() }
+        }
+
+        val updateNeededForArtifact: Pair<Boolean, Artifact> = if (artifacts.empty()) {
+            val artifactDb = Artifact.new {
+                artifactId = artifactDto.artifactId
+                groupId = artifactDto.groupId
+            }
+            Pair(true, artifactDb)
+        } else {
+            val artifactDb = artifacts.first()
+            if (artifactDb.versions.count().toInt() == artifactDto.versions.count()) {
+                Pair(false, artifactDb)
+            } else {
+                Pair(true, artifactDb)
+            }
+        }
+
+        if (updateNeededForArtifact.first) {
+            artifactDto.versions.forEach { version ->
+                if (version.releaseDate != -1L) {
+                    Version.new {
+                        versionNumber = version.versionNumber
+                        releaseDate = version.releaseDate
+                        artifact = updateNeededForArtifact.second
+                    }
+                } else {
+                    println("Not storing version with missing release date $version")
+                }
+            }
+        }
+    }
+        artifactDto.transitiveDependencies.forEach { transitiveDependency ->
+            recursivelyUpdateCache(artifactDto = transitiveDependency)
+        }
+
+}
--- src/main/kotlin/artifact/ArtifactService.kt
+++ src/main/kotlin/artifact/ArtifactService.kt
@@ -58,7 +58,9 @@ class ArtifactService {
                     name = packageRef.id.name
                 )
 
-                createArtifactDto.addVersions(storedVersions)
+                if(metadataDto.versions.count() == storedVersions.count()) {
+                    createArtifactDto.addVersions(storedVersions)
+                }
             }
 
             val versionsWithoutReleaseDate = createArtifactDto.versions.values.filter { it.releaseDate == -1L }
@@ -113,62 +115,3 @@ class ArtifactService {
         }
     }
 }
-
-//    suspend fun updateVersions(versions: List<VersionDto>, artifactDto: ArtifactDto) = dbQuery {
-//        versions.forEach { versionWithoutReleaseDate ->
-//            val releaseDate = artifactDto.versions.find {
-//                it.versionNumber == versionWithoutReleaseDate.versionNumber
-//            }
-//
-//            releaseDate?.releaseDate?.let {
-//                val version = Version.findById(versionWithoutReleaseDate.dbId)
-//                version?.releaseDate = it
-//            }
-//        }
-//    }
-
-//    private suspend fun getOrCreateArtifact(metadataDto: MetadataDto): ArtifactDto = dbQuery {
-//
-//        val artifactQuery = Artifact.find {
-//            Artifacts.artifactId eq metadataDto.artifactId and (Artifacts.groupId eq metadataDto.groupId)
-//        }.with(Artifact::versions)
-//
-//        val artifactModel = if (artifactQuery.empty()) {
-//            Artifact.new {
-//                artifactId = metadataDto.artifactId
-//                groupId = metadataDto.groupId
-//            }
-//        } else {
-//            if (artifactQuery.count() > 1) {
-//                println("WARNING: More than one matching artifact found!")
-//            }
-//            artifactQuery.first()
-//        }
-//
-//        metadataDto.versions.forEach { currentVersion ->
-//            if (artifactModel.versions.find { it.versionNumber == currentVersion } == null) {
-//                println("Creating new version $currentVersion")
-//                Version.new {
-//                    versionNumber = currentVersion
-//                    artifact = artifactModel
-//                }
-//            } else {
-//                println("Version already exists.")
-//            }
-//        }
-//
-//
-//        return@dbQuery ArtifactDto(
-//            dbId = artifactModel.id.value,
-//            artifactId = metadataDto.artifactId,
-//            groupId = metadataDto.groupId,
-//            versions = artifactModel.versions.map {
-//                VersionDto(
-//                    versionNumber = it.versionNumber,
-//                    releaseDate = it.releaseDate,
-//                    dbId = it.id.value
-//                )
-//            }
-//        )
-//    }
-//
